508b430eca0e5b9b0b106d1fff998231a6aa6d0f
pranaylobo <42401887+pranaylobo@users.noreply.github.com>
2020-05-12 20:04:02 +0530

    Update helpers.js

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -66,7 +66,8 @@ export default {
 
 
     getIceServer(){
-        return {iceServers: [{   urls: [ "stun:eu-turn4.xirsys.com" ]}, {   username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",   credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",   urls: [       "turn:eu-turn4.xirsys.com:80?transport=udp",       "turn:eu-turn4.xirsys.com:3478?transport=udp",       "turn:eu-turn4.xirsys.com:80?transport=tcp",       "turn:eu-turn4.xirsys.com:3478?transport=tcp",       "turns:eu-turn4.xirsys.com:443?transport=tcp",       "turns:eu-turn4.xirsys.com:5349?transport=tcp"   ]}]};
+        return {iceServers: [{urls: 'stun:stun.l.google.com:19302'},{urls: 'stun:global.stun.twilio.com:3478?transport=udp'}],sdpSemantics: 'unified-plan'}
+
     },
     
     
